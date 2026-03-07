cb8616b8de37c25b2909e50822a30e291ea40e6c
ji-web534 <239822821+ji-web534@users.noreply.github.com>
2026-03-07 00:00:52 -0300

    Mi primer commit con React y CSS

---
+++ mi-crypto-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Low_bar from './Low_bar.jsx'
+import Main_screen from './main_screen.jsx'
+import High_bar from './high_bar.jsx' 
+function App() {
+
+
+  return (
+    <>
+      <High_bar/>
+      <Main_screen/>
+      <Low_bar/>
+    </>
+  )
+}
+
+export default App
---
+++ mi-crypto-app/src/High_bar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './high_bar.css'
+export default function high_bar() {
+  return (
+    <div className="high-bar">high bar</div>
+  )
+}
