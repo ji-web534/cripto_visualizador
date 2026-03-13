11d751d96bd0a9a73fa0bc318a8c82bf7752ca97
ji-web534 <239822821+ji-web534@users.noreply.github.com>
2026-03-12 22:43:41 -0300

    Refactorización de imports y cambios menores.

--- mi-crypto-app/src/App.jsx
+++ mi-crypto-app/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
-
-import Main_screen from './Main_screen.jsx'
-import High_bar from './high_bar.jsx' 
+import Main_screen from './Main-screen.jsx'
 import { Route, Router, Routes } from 'react-router-dom'
 import './app.css'
 import { Pantalla_inicio } from './Pantalla-inicio.jsx'
+import Side_bar from './side-bar.jsx'
 function App() {
 
 
   return (
     <div className='app-container'>
      
-      <High_bar /> 
+      <Side_bar/> 
     <div className="detalle-container">
         <Routes>
             <Route path="/crypto/:id" element={<Main_screen />} />
