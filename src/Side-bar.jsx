08f258c66ef3993ac514cdad6515eb1581b3c458
ji-web534 <239822821+ji-web534@users.noreply.github.com>
2026-04-01 23:54:12 -0300

    siguiendo con la refactorizacion de estilos css del sidebar

--- src/Side-bar.jsx
+++ src/Side-bar.jsx
@@ -3,22 +3,22 @@ import './side-bar.css'
 import { useCrypto } from './CryptoProvider.jsx';
 import { Link } from 'react-router-dom'
 export default function Side_bar() {
-  const { crypto, loading, error } = useCrypto(); 
+  const { crypto, loading, error, } = useCrypto();
   //creamos la funcion que se encarga de el camio de color
 
 
   const definirColor24h = (variacion) => {
-  if (variacion > 0) return '#22c55e'; // Un verde moderno
-  if (variacion < 0) return '#ef4444'; // Un rojo moderno
-  return '#94a3b8';                    // Gris si no hubo cambio
-};
-  
+    if (variacion > 0) return '#22c55e'; // Un verde moderno
+    if (variacion < 0) return '#ef4444'; // Un rojo moderno
+    return '#94a3b8';                    // Gris si no hubo cambio
+  };
 
-  //  Conectamos el dato con nuestra función
-  const colorDinamico = definirColor24h(cambio);;
-  // 2. Manejamos el estado de carga y error)
+
+
+  // Manejamos el estado de carga y error)
   if (loading) return <div className="side-bar">Cargando criptos...</div>;
   if (error) return <div className="side-bar">Error: {error}</div>;
+  console.log("Estructura de la primera cripto:", crypto[0]);
   return (
 
 
@@ -31,18 +31,19 @@ export default function Side_bar() {
           placeholder="Buscar moneda..."
         />
       </div>
-      {crypto.map(crypto => (
-        <Link to={`/crypto/${crypto.id}`} style={{ textDecoration: 'none', color: 'inherit' }} >
-          <div key={crypto.id} className="crypto-card">
-            <img src={crypto.image} alt={crypto.name} width="30" />
+      {crypto.map(item => (
+        <Link to={`/crypto/${item.id}`} style={{ textDecoration: 'none', color: 'inherit' }} >
+          <div key={item.id} className="crypto-card">
+            <img src={item.image} alt={item.name} width="30" />
             <div className=''>
 
-              <h3>{crypto.name} ({crypto.symbol})</h3>
-              <p>Precio: ${crypto.current_price?.toLocaleString()}</p>
+              <h3>{item.name} ({item.symbol})</h3>
+              <p>Precio: ${item.current_price?.toLocaleString()}</p>
 
 
-              <p style={{  }}>
-                {crypto.cambio_24h >= 0 ? '▲' : '▼'} {Math.abs(crypto.cambio_24h)}%
+
+              <p style={{ color: definirColor24h(item.cambio_24h) }}>
+                {item.cryptocambio_24h >= 0 ? '▲' : '▼'} {Math.abs(item.cambio_24h).toFixed(2)}%
               </p>
 
             </div>
