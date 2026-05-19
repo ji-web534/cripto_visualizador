501d8772dad2980ced272a9593c992ac9cff89da
ji-web534 <239822821+ji-web534@users.noreply.github.com>
2026-05-18 23:59:11 -0300

    finalizacion del css en Details-bar me parece correcto usar css en el codigo por el echo de que es un efecto muy basico y simple y no empeora significativamente la limpieza del codigo

--- src/components/Details-bar.jsx
+++ src/components/Details-bar.jsx
@@ -4,10 +4,27 @@ export function Details_bar({ moneda }) {
     // Si moneda no llega (por si acaso), evitamos que explote
     if (!moneda) return null; 
 
+ 
+    const porcentajeCambio = moneda.price_change_percentage_24h || 0; 
+    
+   
+    const esPositivo = porcentajeCambio >= 0;
+    const colorEstilo = esPositivo ? '#4caf50' : '#ff5252'; 
+
     return (
         <div className="Details-bar">
-            <h1>{moneda.name}</h1>
-            <p>Precio: {moneda.current_price} </p>
+            <div className="info-principal">
+                <h1>{moneda.name}</h1>
+                <p className="precio-texto">
+                    Precio: <span>USD {parseFloat(moneda.current_price).toLocaleString()}</span>
+                </p>
+            </div>
+
+            <div className="porcentaje-caja" style={{ color: colorEstilo }}>
+                <span className="flecha">{esPositivo ? '▲' : '▼'}</span>
+                <span className="numero">{Math.abs(porcentajeCambio).toFixed(2)}%</span>
+            </div>
         </div>
     );
 }
+
