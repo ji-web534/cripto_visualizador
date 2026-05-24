5b005c288c5ea4f12103288e0a9c162c9b6275e4
ji-web534 <239822821+ji-web534@users.noreply.github.com>
2026-05-23 23:53:23 -0300

    ajuste de la altura de la details-bar

--- src/components/CryptoChart.jsx
+++ src/components/CryptoChart.jsx
@@ -74,7 +74,7 @@ export default function CryptoChart({ data }) {
         <div 
             ref={chartContainerRef} 
             style={{ 
-           width: '100vw',      
+           width: '90vw',      
                 height: '100%', 
            
         }}
